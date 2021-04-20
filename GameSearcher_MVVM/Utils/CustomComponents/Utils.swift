a251e4b59c40ee678ce28acaa0e3ae425321be55
Yulia8294 <elenn1156@gmail.com>
2021-04-20 11:25:06 +0300

    Moved utils to helperkit

--- GameSearcher_MVVM/Utils/CustomComponents/Utils.swift
+++ GameSearcher_MVVM/Utils/CustomComponents/Utils.swift
@@ -10,54 +10,6 @@ import UIKit
 import Kingfisher
 import RealmSwift
 
-extension UIView {
-    func snapshot() -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(bounds.size, false, UIScreen.main.scale)
-        drawHierarchy(in: bounds, afterScreenUpdates: true)
-        let result = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return result!
-    }
-}
-
-extension UIWindow {
-    func replaceRootViewControllerWith(_ replacementController: UIViewController, animated: Bool = true, completion: (() -> ())? = nil) {
-        let snapshotImageView = UIImageView(image: self.snapshot())
-        self.addSubview(snapshotImageView)
-        
-        let dismissCompletion = { () -> Void in
-            self.rootViewController = replacementController
-            self.bringSubviewToFront(snapshotImageView)
-            if animated {
-                UIView.animate(withDuration: 0.4, animations: { () -> Void in
-                    snapshotImageView.alpha = 0
-                }, completion: { (success) -> Void in
-                    snapshotImageView.removeFromSuperview()
-                    completion?()
-                })
-            }
-            else {
-                snapshotImageView.removeFromSuperview()
-                completion?()
-            }
-        }
-        if self.rootViewController!.presentedViewController != nil {
-            self.rootViewController!.dismiss(animated: false, completion: dismissCompletion)
-        }
-        else {
-            dismissCompletion()
-        }
-    }
-}
-
-var window: UIWindow {
-    (UIApplication.shared.delegate as! AppDelegate).window!
-}
-
-func setRootController(_ controller: UIViewController) {
-    window.replaceRootViewControllerWith(controller)
-}
-
 //done
 extension String {
     
@@ -88,14 +40,6 @@ func getImageFrom(gradientLayer:CAGradientLayer) -> UIImage? {
      return gradientImage
 }
 
-extension UIColor {
-    func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { rendererContext in
-            self.setFill()
-            rendererContext.fill(CGRect(origin: .zero, size: size))
-        }
-    }
-}
 
 
 public extension Array where Element : Hashable {
