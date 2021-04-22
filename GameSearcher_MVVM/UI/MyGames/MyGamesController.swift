da15950935006eeb89ba13522e3f38a6eabf30a8
Yulia8294 <elenn1156@gmail.com>
2021-04-22 09:31:29 +0300

    Removed logics from MyGamesViewController

--- GameSearcher_MVVM/UI/MyGames/MyGamesController.swift
+++ GameSearcher_MVVM/UI/MyGames/MyGamesController.swift
@@ -18,17 +18,32 @@ class MyGamesViewController: UIViewController {
         
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var games: [GameItemViewModel] = []
+    private var games: [[GameItemViewModel]] {
+        viewModel.items.value
+    }
     
-    private var groupedGames = [[GameItemViewModel]]()
     private var type: PresentationType = .all
     
     private var viewModel: MyGamesViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        assembleGroupedGames()
+        bind(with: viewModel)
+        viewModel.viewDidLoad()
         collectionViewSetup()
+    }
+    
+    func bind(with viewModel: MyGamesViewModel) {
+        viewModel.type.observe(on: self) { [weak self] in self?.onTypeChanged($0) }
+    }
+    
+    private func onTypeChanged(_ type: PresentationType) {
+        switch type {
+        case .all:
+            collectionView.setCollectionViewLayout(MyGamesLayoutManager.createAllGamesLayout(), animated: true)
+        case .byYear:
+            collectionView.setCollectionViewLayout(MyGamesLayoutManager.createSortedLayout(), animated: true)
+        }
     }
     
     static func create(with viewModel: MyGamesViewModel) -> MyGamesViewController {
@@ -43,24 +58,8 @@ class MyGamesViewController: UIViewController {
         collectionView.registerHeader(HeaderView.self)
     }
     
-    private func assembleGroupedGames() {
-        let groupedUsers = Dictionary(grouping: games) { $0.released.anyString }
-        let sortedKeys = groupedUsers.keys.sorted { $0 > $1 }
-        sortedKeys.forEach { (key) in
-            if let values = groupedUsers[key] {
-                groupedGames.append(values)
-            }
-        }
-    }
-    
     @IBAction func didPressSwitchLayout(_ sender: UIBarButtonItem) {
-        if type == .all {
-            type = .byYear
-            collectionView.setCollectionViewLayout(MyGamesLayoutManager.createSortedLayout(), animated: true)
-        } else if type == .byYear {
-            type = .all
-            collectionView.setCollectionViewLayout(MyGamesLayoutManager.createAllGamesLayout(), animated: true)
-        }
+        viewModel.didChangePresentationType()
     }
 }
 
@@ -68,16 +67,11 @@ class MyGamesViewController: UIViewController {
 extension MyGamesViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        switch type {
-        case .all:
-            return collectionView.cell(SimilarGamesCell.self, for: indexPath).setupGame(games[indexPath.row])
-        case .byYear:
-            return collectionView.cell(SimilarGamesCell.self, for: indexPath).setupGame(groupedGames[indexPath.section][indexPath.row])
-        }
+        collectionView.cell(SimilarGamesCell.self, for: indexPath).setupGame(games[indexPath.section][indexPath.row])
     }
         
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        type == .all ? games.count : groupedGames[section].count
+        games[section].count
     }
     
 }
@@ -85,7 +79,7 @@ extension MyGamesViewController: UICollectionViewDataSource {
 extension MyGamesViewController: UICollectionViewDelegate {
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-       
+        viewModel.didSelectItem(at: indexPath)
     }
 }
 
@@ -93,12 +87,12 @@ extension MyGamesViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         if type == .all { return UICollectionReusableView() }
-        let firstItemInSectionYear = groupedGames[indexPath.section].first!.released
+        let firstItemInSectionYear = games[indexPath.section].first!.released
         return collectionView.header(HeaderView.self, for: indexPath).setup(String(firstItemInSectionYear.anyString))
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return type == .all ? 1 : groupedGames.count
+        return games.count
     }
 }
 
--- GameSearcher_MVVM/UI/MyGames/ViewModel/MyGamesViewModel.swift
+++ GameSearcher_MVVM/UI/MyGames/ViewModel/MyGamesViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 struct MyGamesViewModelActions {
     let showGameDetails: (GameItem) -> Void
@@ -15,40 +16,57 @@ protocol MyGamesViewModel: MyGamesViewModelInput, MyGamesViewModelOutput { }
 
 protocol MyGamesViewModelInput {
     func viewDidLoad()
-    func didSelectItem(at index: Int)
-    
+    func didSelectItem(at IndexPath: IndexPath)
+    func didChangePresentationType()
 }
 
 protocol MyGamesViewModelOutput {
     var screenTitle: String { get }
-    var items: Observable<[GameItemViewModel]> { get }
+    var items: Observable<[[GameItemViewModel]]> { get }
+    var type: Observable<PresentationType> { get }
 }
 
-
-
-
 final class DefaultMyGamesViewModel: MyGamesViewModel {
-    
+
     private var actions: MyGamesViewModelActions?
+    
+    private var games: Results<GameItem> {
+        RealmService.shared.objects(GameItem.self)
+    }
+    private var groupedGames: [[GameItem]] = [[]]
+
 
     init(actions: MyGamesViewModelActions) {
         self.actions = actions
     }
     
-    var screenTitle: String = ""
-    var items: Observable<[GameItemViewModel]> = Observable([])
+    var screenTitle: String = "My Games"
     
-    
+    var items: Observable<[[GameItemViewModel]]> = Observable([])
+    var type: Observable<PresentationType> = Observable(.all)
+
     func viewDidLoad() {
+        assembleGroupedGames()
+    }
+    
+    func didSelectItem(at indexPath: IndexPath) {
         
     }
     
-    func didSelectItem(at index: Int) {
-    
+    func didChangePresentationType() {
+        
     }
     
-  
+//MARK: - Private
     
-    
-    
+    private func assembleGroupedGames() {
+        let groupedUsers = Dictionary(grouping: games) { $0.releaseYear }
+        let sortedKeys = groupedUsers.keys.sorted { $0 > $1 }
+        sortedKeys.forEach { (key) in
+            if let values = groupedUsers[key] {
+                groupedGames.append(values)
+                items.value.append(values.map{ GameItemViewModel(game: $0)})
+            }
+        }
+    }
 }
